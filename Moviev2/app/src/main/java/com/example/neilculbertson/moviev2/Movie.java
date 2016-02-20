692ade836a3739d0768ac29de630b6314c7334f0
njculber <njculber@mtu.edu>
2016-02-20 00:52:35 -0500

    This is the current UI for the Moomie app. Woohoo

---
+++ Moviev2/app/src/main/java/com/example/neilculbertson/moviev2/Movie.java
@@ -0,0 +1,26 @@
+package com.example.neilculbertson.moviev2;
+
+/**
+ * Created by neilculbertson on 2/20/16.
+ */
+public class Movie {
+    private String [] grades= {"A", "A-", "B-", "B", "B+", "C-", "C", "C+", "D-", "D", "D+", "E-", "E", "E+", "F"};
+    private String movieTitle;
+    private String movieGrade;
+
+    public String getMovietitle(){
+        return movieTitle;
+    }
+
+    public void setMovieTitle(String newMovie){
+        movieTitle = newMovie;
+    }
+
+    public String getMovieGrade(){
+        return movieGrade;
+    }
+
+    public void setMovieGrade(String newGrade){
+        movieGrade = newGrade;
+    }
+}
